b0662df8d75ca3e0b41958b9d2e914051e36d795
lamafout <dzordzvashington@gmail.com>
2024-08-02 16:42:29 +0300

    i returned to the Stack for new cards widget. Started to add draggable cards

--- lib/widgets/new_notes_stack.dart
+++ lib/widgets/new_notes_stack.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_tindercard_plus/flutter_tindercard_plus.dart';
 import './osa_note_card.dart';
 
 class NewNotesList extends StatefulWidget {
@@ -17,29 +16,28 @@ class _NewNotesListState extends State<NewNotesList> {
     return Container(
       width: double.infinity,
       height: 700,
-      //TODO исправить краш, пока не понимаю сути проблемы
-      child: TinderSwapCard(
-        totalNum: widget.cards.length,
-        stackNum: 5,
-        swipeUp: false,
-        swipeDown: false,
-        cardBuilder: (context, index) => widget.cards[index],
-        swipeCompleteCallback: (CardSwipeOrientation orientation, int index) {
-          setState(() {
-            widget.cards.removeAt(index);
-          });
-        },
-        // child: Stack(
-        //   children: [
-        //     ...widget.cards.reversed.toList().asMap().entries.map((entry) {
-        //       return Container(
-        //         margin: EdgeInsets.only(top: 10*entry.key.toDouble()),
-        //         child: entry.value,
-        //       );
-        //     })
-        //   ],
-        // ),
-      ),
-    );
+      //TODO вернутсья на использование стека и анимированного контейнера
+      child: Stack(
+          children: [
+            ...widget.cards.reversed.toList().asMap().entries.map((entry) {
+              return entry.key == 0
+              ? Draggable(
+                feedback: Container(
+                  margin: EdgeInsets.only(top: 10*entry.key.toDouble()),
+                  child: entry.value,
+                  ),
+                child: Container(
+                  margin: EdgeInsets.only(top: 10*entry.key.toDouble()),
+                  child: entry.value,
+                  ), 
+                )
+              : Container(
+                margin: EdgeInsets.only(top: 10*entry.key.toDouble()),
+                child: entry.value,
+                );
+            })
+          ],
+        ),
+      );
   }
 }
